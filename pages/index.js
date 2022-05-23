dd4c6a20c95a8a931fa1082ee3df9bd85c7fdbce
Abriele Qudsi <dev.qudsi@gmail.com>
2022-05-23 12:35:20 +0100

    feat: add HeroSection

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
+import HeroSection from '../components/sections/HeroSection'
+
 export default function Home() {
   return (
     <section className="bg-gradient-to-r from-cyan-500 to-blue-500">
-      <h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <HeroSection />
     </section>
   )
 }
