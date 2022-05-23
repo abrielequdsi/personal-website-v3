86c41d66e2ead90f86cfd770d41aa16b47674350
Abriele Qudsi <dev.qudsi@gmail.com>
2022-05-23 12:34:14 +0100

    style: add inter font

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Abriele Qudsi</title>
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
