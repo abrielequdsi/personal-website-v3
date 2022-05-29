0e02b5eaf092ce8c4f4c20556cf5a54691e7ab0d
Abriele Qudsi <dev.qudsi@gmail.com>
2022-05-29 20:56:16 +0100

    refactor: remove comments

--- src/components/main-page/ContactSection/ContactSection.js
+++ src/components/main-page/ContactSection/ContactSection.js
@@ -9,19 +9,6 @@ const ContactSection = () => {
           <div className="text-left sm:text-center text-indigo-600 font-medium">
             04. Contact
           </div>
-          {/* <div className="grid grid-cols-3 items-center mt-2">
-            <div className="hidden sm:block sm:pr-5">
-              <hr />
-            </div>
-            <div className="col-span-2 sm:col-span-1 text-left sm:text-center">
-              <h2 className="text-3xl  lg:text-4xl font-extrabold  text-gray-900">
-                Get In Touch
-              </h2>
-            </div>
-            <div className="hidden sm:block sm:pl-5">
-              <hr />
-            </div>
-          </div> */}
           <div className="relative flex mt-2 mb-0 sm:mb-5 items-center">
             <div className="flex-grow border-t border-gray-200 hidden sm:block "></div>
             <h1 className="flex-shrink mx-0 sm:mx-4 text-3xl  lg:text-4xl font-extrabold  text-gray-900">
@@ -29,31 +16,6 @@ const ContactSection = () => {
             </h1>
             <div className="flex-grow border-t border-gray-20 hidden sm:block "></div>
           </div>
-          {/* <div className="grid grid-cols-3 items-center mt-2">
-            <div className="hidden sm:block sm:pr-5">
-              <hr />
-            </div>
-            <div className="col-span-2 sm:col-span-1 text-left sm:text-center">
-              <h2 className="text-3xl  lg:text-4xl font-extrabold  text-gray-900">
-                Get In Touch
-              </h2>
-            </div>
-            <div className="hidden sm:block sm:pl-5">
-              <hr />
-            </div>
-          </div> */}
-          {/* <div className="relative flex pt-5 items-center mt-1">
-            <div className="">
-              <h2 className="flex-shrink mr-4 text-2xl sm:text-3xl font-extrabold  text-gray-900 ">
-                <span className=" text-indigo-600 font-semibold text-xl sm:text-2xl pb-1">
-                  0{number}.
-                </span>{' '}
-                {title}
-              </h2>
-            </div>
-            <div className="flex-grow border-t max-w-md border-gray-300" />
-          </div> */}
-          {/* mt 6 */}
           <div>
             {' '}
             <p className="mt-4 md:mt-6 px-0 md:px-16 text-md lg:text-lg text-gray-500 text-start sm:text-center">
