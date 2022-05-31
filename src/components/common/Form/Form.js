732c4dcc89467ee2dc5ed51fd2fff736d456bca0
Abriele Qudsi <dev.qudsi@gmail.com>
2022-06-01 00:56:28 +0100

    feat: implement submit-form api

--- src/components/common/Form/Form.js
+++ src/components/common/Form/Form.js
@@ -1,99 +1,143 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Alert } from '@components/ui'
 
 const Form = () => {
-  return (
-    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
-      <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 border">
-        <div className="lg:col-span-2">
-          <form
-            action="#"
-            method="POST"
-            className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
-          >
-            <div>
-              <label
-                htmlFor="first-name"
-                className="block text-sm font-medium text-gray-900"
-              >
-                Full Name
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  className="py-3 px-4 block w-full shadow-sm text-sm lg:text-base text-gray-900 focus:outline-none focus:ring-1 focus:ring-vampire-black focus:border-vampire-black border-gray-300 rounded-md border"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-900"
-              >
-                Email
-              </label>
-              <div className="mt-1">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  className="py-3 px-4 block w-full text-sm lg:text-base shadow-sm text-gray-900 focus:outline-none focus:ring-1 focus:ring-vampire-black focus:border-vampire-black border-gray-300 rounded-md border"
-                />
-              </div>
-            </div>
+  const [alert, setAlert] = useState('')
+  // Input states
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
 
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="subject"
-                className="block text-sm font-medium text-gray-900"
-              >
-                Subject
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="subject"
-                  id="subject"
-                  className="py-3 px-4 block w-full shadow-sm text-sm lg:text-base text-gray-900 focus:outline-none focus:ring-1 focus:ring-vampire-black focus:border-vampire-black border-gray-300 rounded-md border"
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <div className="flex justify-between">
+  const clearAlert = () => {
+    setAlert('')
+  }
+
+  // Form submit handler
+  const submitForm = async (e) => {
+    e.preventDefault()
+    const res = await fetch('http://localhost:3000/api/submit-form', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, subject, message }),
+    })
+    // Success if status code is 201
+    if (res.status === 201) {
+      setAlert('success')
+    } else {
+      setAlert('fail')
+    }
+    setName('')
+    setEmail('')
+    setSubject('')
+    setMessage('')
+  }
+
+  return (
+    <div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 border">
+          <div className="lg:col-span-2">
+            <form
+              className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+              onSubmit={submitForm}
+            >
+              <div>
                 <label
-                  htmlFor="message"
+                  htmlFor="first-name"
                   className="block text-sm font-medium text-gray-900"
                 >
-                  Message
+                  Full Name
                 </label>
-                <span id="message-max" className="text-sm text-x2-grey">
-                  Max. 500 characters
-                </span>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="full-name"
+                    id="full-name"
+                    autoComplete="full-name"
+                    className="py-3 px-4 block w-full shadow-sm text-sm lg:text-base text-gray-900 focus:outline-none focus:ring-1 focus:ring-vampire-black focus:border-vampire-black border-gray-300 rounded-md border"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </div>
               </div>
-              <div className="mt-1">
-                <textarea
-                  id="message"
-                  name="message"
-                  rows={4}
-                  className="py-3 px-4 block w-full shadow-sm text-sm lg:text-base text-gray-900 focus:outline-none focus:ring-1 focus:ring-vampire-black focus:border-vampire-black border border-gray-300 rounded-md"
-                  aria-describedby="message-max"
-                  defaultValue={''}
-                />
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="py-3 px-4 block w-full text-sm lg:text-base shadow-sm text-gray-900 focus:outline-none focus:ring-1 focus:ring-vampire-black focus:border-vampire-black border-gray-300 rounded-md border"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <div className="sm:col-span-2 sm:flex sm:justify-end">
-              <button
-                type="submit"
-                className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent  font-medium rounded-md text-white bg-vampire-black shadow-sm hover:ring-vampire-black hover:outline-none hover:ring-2 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-vampire-black sm:mt-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto text-base animated"
-              >
-                Let&apos;s talk
-              </button>
-            </div>
-          </form>
+
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="subject"
+                  className="block text-sm font-medium text-gray-900"
+                >
+                  Subject
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="subject"
+                    name="subject"
+                    type="text"
+                    className="py-3 px-4 block w-full shadow-sm text-sm lg:text-base text-gray-900 focus:outline-none focus:ring-1 focus:ring-vampire-black focus:border-vampire-black border-gray-300 rounded-md border"
+                    value={subject}
+                    onChange={(e) => setSubject(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <div className="flex justify-between">
+                  <label
+                    htmlFor="message"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    Message
+                  </label>
+                  <span id="message-max" className="text-sm text-x2-grey">
+                    Max. 500 characters
+                  </span>
+                </div>
+                <div className="mt-1">
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows={4}
+                    className="py-3 px-4 block w-full shadow-sm text-sm lg:text-base text-gray-900 focus:outline-none focus:ring-1 focus:ring-vampire-black focus:border-vampire-black border border-gray-300 rounded-md"
+                    aria-describedby="message-max"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2 sm:flex sm:justify-end">
+                <button
+                  type="submit"
+                  className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent  font-medium rounded-md text-white bg-vampire-black shadow-sm hover:ring-vampire-black hover:outline-none hover:ring-2 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-vampire-black sm:mt-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto text-base animated"
+                >
+                  Let&apos;s talk
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+        <Alert status={alert} clearAlert={clearAlert} />
       </div>
     </div>
   )
---
+++ src/components/ui/Alert/Alert.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react'
+import { XIcon } from '@heroicons/react/solid'
+
+const Alert = ({ status, clearAlert }) => {
+  //   const [showAlert, setShowAlert] = useState(true)
+
+  const toggleAlert = () => {
+    // setShowAlert(false)
+    clearAlert()
+    // setShowAlert(true)
+    // setShowAlert()
+  }
+
+  console.log('status', status)
+  //   console.log('alert', showAlert)
+
+  return (
+    <>
+      {(() => {
+        if (status !== '') {
+          if (status == 'success') {
+            return (
+              <div className="rounded-md bg-green-100 p-4 mt-6">
+                <div className="flex">
+                  <div className="ml-3">
+                    <p className="text-sm font-medium text-green-800">
+                      Thank you for contacting!
+                    </p>
+                  </div>
+                  <div className="ml-auto pl-3">
+                    <div className="-mx-1.5 -my-1.5">
+                      <button
+                        type="button"
+                        className="inline-flex bg-green-100 rounded-md p-1.5 text-green-500 hover:bg-green-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
+                        onClick={toggleAlert}
+                      >
+                        <span className="sr-only">Dismiss</span>
+                        <XIcon className="h-5 w-5" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          } else {
+            return (
+              <div className="rounded-md bg-red-100 p-4 mt-6">
+                <div className="flex">
+                  <div className="ml-3">
+                    <p className="text-sm font-medium text-red-800">
+                      Message Failed to Send
+                    </p>
+                  </div>
+                  <div className="ml-auto pl-3">
+                    <div className="-mx-1.5 -my-1.5">
+                      <button
+                        type="button"
+                        className="inline-flex bg-red-100 rounded-md p-1.5 text-red-500 hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-50 focus:ring-red-600"
+                        onClick={toggleAlert}
+                      >
+                        <span className="sr-only">Dismiss</span>
+                        <XIcon className="h-5 w-5" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        }
+      })()}
+    </>
+  )
+}
+
+export default Alert
