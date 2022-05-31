1442f8284a952104c535e5a9bb514124df33b918
Abriele Qudsi <dev.qudsi@gmail.com>
2022-05-31 16:17:31 +0100

    fix: fix navbar logo href ref

--- src/components/common/Navbar/Navbar.js
+++ src/components/common/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import Link from 'next/link'
 
 const navigation = [
   { name: 'About', href: '/#about' },
@@ -18,13 +19,15 @@ const Navbar = () => {
       >
         <div className="flex items-center justify-between flex-1">
           <div className="flex items-center justify-between w-full md:w-auto">
-            <a href="#">
-              <img
-                className="h-10 w-auto sm:h-11 hover:opacity-80"
-                src="main_logo.svg"
-                alt="Main Logo"
-              />
-            </a>
+            <Link href="/#">
+              <a>
+                <img
+                  className="h-10 w-auto sm:h-11 hover:opacity-80"
+                  src="main_logo.svg"
+                  alt="Main Logo"
+                />
+              </a>
+            </Link>
             <div className="-mr-2 flex items-center md:hidden">
               <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-chinese-black hover:text-vampire-black hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-vampire-black">
                 <span className="sr-only">Open main menu</span>
