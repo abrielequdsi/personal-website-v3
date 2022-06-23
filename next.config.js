56ecf42ef63604d62d2d8ddf2eeaa3738b65df5b
Abriele Qudsi <dev.qudsi@gmail.com>
2022-06-23 16:07:18 +0100

    feat: remove unused domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['media-exp1.licdn.com', 's3.us-west-2.amazonaws.com'],
+    domains: ['s3.us-west-2.amazonaws.com'],
   },
   reactStrictMode: true,
 }
